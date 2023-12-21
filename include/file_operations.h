593956dccaf3067af6c61a203a764c21e355a480
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2023-12-21 07:59:22 +0000

    Updated the data validation system

--- include/file_operations.h
+++ include/file_operations.h
@@ -4,12 +4,37 @@
 #ifndef FILE_OPERATIONS_H
 #define FILE_OPERATIONS_H
 
+#include <stdio.h>
+
 #include "sudoku.h"
+
+#define FILE_BUFFER 256
+#define USER_BUFFER 40
+#define MIN_DATA_VALUE 0
+
+typedef struct {
+    FILE *file;
+    int games_played, games_won, success_rate;
+    int line_number, problem_count;
+} FileData;
 
 void generate_data_file(); // DONE
 void validate_data_file();
-char *trim(char *str);
+void validate_statistics(FileData *data);
+void validate_problems(FileData *data);
+void validate_problem_grid(FileData *data, int grid[GRID_SIZE]);
+FILE *open_file(const char *filename, const char *mode);
+void check_int_range(FileData *data, int value, int max, const char *message);
+void check_success_rate(FileData *data, int value, int max, const char *message);
+char *read_line(FileData *data, char *buffer, int buffer_size);
+void exit_error_message(FileData *data, const char *message, int print_check, int extra_values[2]);
+FileData init_file_context(const char *filename);
+void validate_problem_count(FileData *data);
+void validate_total_games_across_problems(FileData *data, int games_played_across_problems);
+
 int count_problems();
 void load_problems(SudokuProblem problems[]);
+
+char *trim(char *str);
 
 #endif
--- src/file_operations.c
+++ src/file_operations.c
@@ -9,6 +9,43 @@
 #include <string.h>
 #include <ctype.h>
 
+FILE *open_file(const char *filename, const char *mode) {
+    FILE *file = fopen(filename, mode);
+    if(!file) {
+        printf("Failed to open %s file.\n", filename);
+        exit(1);
+    }
+    return file;
+}
+
+void check_int_range(FileData *data, int value, int max, const char *message) {
+    if(value < MIN_DATA_VALUE || value > max) {
+        exit_error_message(data, message, -1, NULL);
+    }
+}
+
+void check_success_rate(FileData *data, int value, int max, const char *message) {
+    int calculated_success_rate = (int)((float)data->games_won / (float)data->games_played * 10000);
+    if (calculated_success_rate != value) {
+        message = "Mismatching success rate. Should be";
+        exit_error_message(data, message, calculated_success_rate, NULL);
+    } else if (value < MIN_DATA_VALUE || value > max) exit_error_message(data, message, -1, NULL);
+}
+
+char *read_line(FileData *data, char *buffer, int buffer_size) {
+    char *comment, *trimmed;
+    do {
+        if (fgets(buffer, buffer_size, data->file) == NULL) {
+            return NULL;
+        }
+        data->line_number++;
+        comment = strstr(buffer, "//");
+        if(comment) *comment = '\0';
+        trimmed = trim(buffer);
+    } while(trimmed[0] == '\0');
+    return trimmed;
+}
+
 void generate_data_file() {
     FILE *file = fopen("../data.txt", "r");
     if(file) fclose(file);
@@ -27,109 +64,107 @@ void generate_data_file() {
 }
 
 void validate_data_file() {
-    FILE *file = fopen("../data.txt", "r");
-    if(!file) {
-        printf("Failed to open data.txt file.\n");
-        exit(1);
-    }
+    FileData data = init_file_context("../data.txt");
+    validate_statistics(&data);
+    validate_problems(&data);
+    validate_problem_count(&data);
+    fclose(data.file);
+}
 
-    char buffer[256], problem_name[40];
-    int games_played, games_won, times_played;
-    float success_rate;
-
-    // Read and validate the game statistics line
-    fgets(buffer, sizeof(buffer), file);
-    char *comment = strstr(buffer, "//");
-    if (comment) *comment = '\0'; // Remove trailing comment
-    char *trimmed = trim(buffer); // Trim leading and trailing spaces
+void validate_statistics(FileData *data) {
+    char buffer[FILE_BUFFER];
+    char *trimmed = read_line(data, buffer, sizeof(buffer));
+    int extra, decimal_part;
     printf("Debug: Game statistics line after removing comments and trimming: '%s'\n", trimmed);
-    if (sscanf(trimmed, "%d %d %f", &games_played, &games_won, &success_rate) != 3) {
-        printf("Invalid game statistics line.\n");
-        exit(1);
+    if(sscanf(trimmed, "%d %d %d.%d%n", &(data->games_played), &(data->games_won), &(data->success_rate), &decimal_part, &extra) != 4 || trimmed[extra] != '\0') {
+        exit_error_message(data, "Invalid game statistics line.", -1, NULL);
     }
-    if (games_played < games_won) {
-        printf("Number of games played is less than number of games won.\n");
-        exit(1);
-    }
-    if (games_played < 0) {
-        printf("Number of games played is less than 0.\n");
-        exit(1);
-    }
-    if (games_won < 0) {
-        printf("Number of games won is less than 0.\n");
-        exit(1);
-    }
-    if (success_rate < 0 || success_rate > 100) {
-        printf("Success rate is not between 0 and 100.\n");
-        exit(1);
-    }
-    printf("Debug: Games played: %d, Games won: %d, Success rate: %.2f\n", games_played, games_won, success_rate);
+    data->success_rate = data->success_rate * 100 + decimal_part;
+    check_int_range(data, decimal_part, 99, "Success rate has an invalid decimal part");
+    check_int_range(data, data->games_played, INT_MAX, "Number of games played is less than 0.");
+    check_int_range(data, data->games_won, data->games_played, "Number of games won is less than 0 or greater than number of games played.");
+    check_success_rate(data, data->success_rate, 10000, "Success rate is less than 0 or greater than 100.");
+    printf("Debug: Games played: %d, Games won: %d, Success rate: %.2f\n", data->games_played, data->games_won, (float)(data->success_rate) / 100);
+}
 
-    // Read and validate the problems
-    while (fgets(buffer, sizeof(buffer), file)) {
-        // Remove trailing comment
-        comment = strstr(buffer, "//");
-        if (comment) *comment = '\0';
-
-        // Trim leading and trailing spaces
-        trimmed = trim(buffer);
-
-        // Ignore empty lines
-        if (trimmed[0] == '\0') {
-            continue;
+void validate_problem_grid(FileData *data, int grid[GRID_SIZE]) {
+    char buffer[FILE_BUFFER];
+    for(int i = 0; i < GRID_SIZE; i++) {
+        char *trimmed = read_line(data, buffer, sizeof(buffer));
+        int extra;
+        printf("Debug: Grid line after removing comments and trimming: '%s'\n", trimmed);
+        if(sscanf(trimmed, "%d %d %d %d%n", &grid[0], &grid[1], &grid[2], &grid[3], &extra) != 4 ||trimmed[extra] != '\0') {
+            exit_error_message(data, "Invalid grid line.", -1, NULL);
         }
+        for(int j = 0; j < GRID_SIZE; j++) check_int_range(data, grid[j], GRID_SIZE, "Invalid grid value.");
+        printf("Debug: Grid values: %d, %d, %d, %d\n", grid[0], grid[1], grid[2], grid[3]);
+    }
+}
 
+void validate_problems(FileData *data) {
+    char buffer[FILE_BUFFER], problem_name[USER_BUFFER], *trimmed;
+    int times_played, grid[GRID_SIZE], games_played_across_problems = 0;
+    while((trimmed = read_line(data, buffer, sizeof(buffer))) != NULL) {
         printf("Debug: Problem line after removing comments and trimming: '%s'\n", trimmed);
-
-        // Validate the problem name and times played
         if (sscanf(trimmed, "\"%[^\"]\" %d", problem_name, &times_played) != 2 || times_played < 0) {
-            printf("Invalid problem line.\n");
-            exit(1);
+            exit_error_message(data, "Invalid problem line.", -1, NULL);
         }
         printf("Debug: Problem name: '%s', Times played: %d\n", problem_name, times_played);
-
-        // Validate the problem grid
-        for (int i = 0; i < 4; i++) {
-            fgets(buffer, sizeof(buffer), file);
-            comment = strstr(buffer, "//");
-            if (comment) *comment = '\0';
-            trimmed = trim(buffer);
-            printf("Debug: Grid line after removing comments and trimming: '%s'\n", trimmed);
-            int grid[4];
-            if (sscanf(trimmed, "%d %d %d %d", &grid[0], &grid[1], &grid[2], &grid[3]) != 4) {
-                printf("Invalid grid line.\n");
-                exit(1);
-            }
-            for (int j = 0; j < 4; j++) {
-                if (grid[j] < 0 || grid[j] > 4) {
-                    printf("Invalid grid value.\n");
-                    exit(1);
-                }
-            }
-            printf("Debug: Grid values: %d, %d, %d, %d\n", grid[0], grid[1], grid[2], grid[3]);
-        }
+        validate_problem_grid(data, grid);
+        games_played_across_problems += times_played;
+        data->problem_count++;
     }
-
-    fclose(file);
+    validate_total_games_across_problems(data, games_played_across_problems);
 }
 
 char *trim(char *str) {
     char *end;
-
-    // Trim leading space
     while(isspace((unsigned char)*str)) str++;
-
-    if(*str == 0)  // All spaces?
-        return str;
-
-    // Trim trailing space
+    if(*str == 0) return str;
     end = str + strlen(str) - 1;
     while(end > str && isspace((unsigned char)*end)) end--;
-
-    // Write new null terminator character
     end[1] = '\0';
-
     return str;
+}
+
+void exit_error_message(FileData *data, const char *message, int print_check, int extra_values[2]) {
+    if(print_check == -1) printf("Error on line %d: %s\n", data->line_number, message);
+    else if(print_check == -2) {
+        if(extra_values[0] < extra_values[1]) {
+            printf("%s", message);
+            printf(" Expected %d at most ", extra_values[0]);
+            printf("but found %d.\n", extra_values[1]);
+        }
+        else {
+            printf("%s", message);
+            printf(" Expected %d ", extra_values[0]);
+            printf("but found %d.\n", extra_values[1]);
+        }
+    }
+    else printf("Error on line %d: %s %.2f\n", data->line_number, message, (float)(print_check) / 100);
+    fclose(data->file);
+    exit(1);
+}
+
+FileData init_file_context(const char *filename) {
+    FileData data;
+    data.file = open_file(filename, "r");
+    data.line_number = data.games_played = data.games_won = data.success_rate = 0;
+    return data;
+}
+
+void validate_problem_count(FileData *data) {
+    if(data->problem_count > MAX_PROBLEMS) {
+        int extra_values[2] = {MAX_PROBLEMS, data->problem_count};
+        exit_error_message(data, "Maximum number of problems exceeded.", -2, extra_values);
+    }
+}
+
+void validate_total_games_across_problems(FileData *data, int games_played_across_problems) {
+    if(data->games_played != games_played_across_problems) {
+        int extra_values[2] = {data->games_played, games_played_across_problems};
+        exit_error_message(data, "Mismatching total number of games played.", -2, extra_values);
+    }
 }
 
 //int count_problems() {
